8349b4a68b41653a3a4fe5b1308113c5382b7ec3
Ivan Novozhilov <nip1992@mail.ru>
2023-10-26 21:03:40 +0300

    log error

--- hw01/src/main/java/ru/otus/spring/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/spring/service/TestServiceImpl.java
@@ -1,12 +1,17 @@
 package ru.otus.spring.service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ru.otus.spring.dao.QuestionDao;
 import ru.otus.spring.domain.Answer;
 import ru.otus.spring.domain.Question;
 
 public class TestServiceImpl implements TestService {
+
+    private final static Logger logger = Logger.getLogger(TestServiceImpl.class.getName());
+
     private final IOService ioService;
 
     private final QuestionDao questionDao;
@@ -21,7 +26,7 @@ public class TestServiceImpl implements TestService {
         try {
             printTest(questionDao.findAll());
         } catch (Exception e) {
-            throw new Error("Ошибка чтения вопросов для теста", e.getCause());
+            logger.severe("Ошибка чтения вопросов для теста" + e);
         }
     }
 
