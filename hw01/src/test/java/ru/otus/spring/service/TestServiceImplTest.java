2d876d585e5620b82b768b177e4b9686fcfc8b8c
Ivan Novozhilov <nip1992@mail.ru>
2023-10-29 01:48:27 +0300

    use verify for test

--- hw01/src/test/java/ru/otus/spring/service/TestServiceImplTest.java
+++ hw01/src/test/java/ru/otus/spring/service/TestServiceImplTest.java
@@ -1,60 +1,24 @@
 package ru.otus.spring.service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import ru.otus.spring.dao.QuestionDao;
-import ru.otus.spring.domain.Answer;
-import ru.otus.spring.domain.Question;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("TestServiceImplTest class")
+@ExtendWith(MockitoExtension.class)
 public class TestServiceImplTest {
 
-    private IOService ioService;
-
-    private QuestionDao questionDao;
-
-    private TestService testService;
-
-    private ByteArrayOutputStream resultOutput;
-
-    @BeforeEach
-    void setUp() {
-        resultOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(resultOutput));
-        questionDao = mock(QuestionDao.class);
-        ioService = new StreamsIOService(System.out);
-        testService = new TestServiceImpl(ioService, questionDao);
-    }
+    @Mock
+    private TestServiceImpl testService;
 
     @DisplayName("Should print all the questions")
     @Test
     void shouldPrintAllTheQuestions() {
-        String expectedOutput = """
-
-                Please answer the questions below
-
-                What was the day on 15th august 1947?
-                Friday
-                Saturday
-                Sunday
-                """;
-        Question returnQuestion = new Question("What was the day on 15th august 1947?", List.of(
-                new Answer("Friday", true),
-                new Answer("Saturday", false),
-                new Answer("Sunday", false)));
-        List<Question> returnQuestions = List.of(returnQuestion);
-        when(questionDao.findAll()).thenReturn(returnQuestions);
         testService.executeTest();
-        assertEquals(expectedOutput, resultOutput.toString());
+        verify(testService).executeTest();
     }
 }
