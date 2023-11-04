80b45f20fa0e9b6d8bbf53bb30f759cef8a15265
Ivan Novozhilov <nip1992@mail.ru>
2023-11-05 01:10:20 +0300

    custom banner; use SpringBootApplication.run

--- hw03/src/main/java/ru/otus/spring/service/TestRunnerService.java
+++ hw03/src/main/java/ru/otus/spring/service/TestRunnerService.java
@@ -1,6 +1,30 @@
 package ru.otus.spring.service;
 
-public interface TestRunnerService {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import ru.otus.spring.domain.Student;
+import ru.otus.spring.domain.TestResult;
+
+@Service
+public class TestRunnerService implements CommandLineRunner {
     
-    void run();
+    private final TestService testService;
+
+    private final StudentService studentService;
+
+    private final ResultService resultService;
+
+    public TestRunnerService(TestService testService, StudentService studentService, ResultService resultService) {
+        this.testService = testService;
+        this.studentService = studentService;
+        this.resultService = resultService;
+    }
+
+    @Override
+    public void run(String... args) {
+        Student student = studentService.determineCurrentStudent();
+        TestResult testResult = testService.executeTestFor(student);
+        resultService.showResult(testResult);
+    }
 }
