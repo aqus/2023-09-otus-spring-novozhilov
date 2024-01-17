928a6cd74bae03c231a917f3a503b2f7f3e1918b
Ivan Novozhilov <nip1992@mail.ru>
2024-01-17 21:49:27 +0300

    test configuration

--- hw10-spring-mvc/src/test/java/ru/otus/catalog/ApplicationTests.java
+++ hw10-spring-mvc/src/test/java/ru/otus/catalog/ApplicationTests.java
@@ -2,9 +2,15 @@ package ru.otus.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class ApplicationTests {
+    
+    @Configuration
+    public static class TestConfiguration {
+        
+    }
 
     @Test
     void contextLoads() {
