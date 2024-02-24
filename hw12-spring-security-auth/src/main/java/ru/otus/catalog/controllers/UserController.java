d60019f56c7b1ebb8b3489618c52c1e7f81c075a
Ivan Novozhilov <nip1992@mail.ru>
2024-02-25 01:14:48 +0300

    add AuthService

--- hw12-spring-security-auth/src/main/java/ru/otus/catalog/controllers/UserController.java
+++ hw12-spring-security-auth/src/main/java/ru/otus/catalog/controllers/UserController.java
@@ -2,63 +2,23 @@ package ru.otus.catalog.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.catalog.dto.UserLoginDto;
-import ru.otus.catalog.security.UserService;
-
-import java.time.Instant;
-import java.util.stream.Collectors;
+import ru.otus.catalog.services.AuthService;
 
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
-    private final DaoAuthenticationProvider daoAuthenticationProvider;
-
-    private final JwtEncoder encoder;
-
-    public UserController(UserService userService, DaoAuthenticationProvider daoAuthenticationProvider,
-                          JwtEncoder encoder) {
-        this.userService = userService;
-        this.daoAuthenticationProvider = daoAuthenticationProvider;
-        this.encoder = encoder;
+    public UserController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("api/v1/authenticate")
     public ResponseEntity<String> authenticate(@RequestBody UserLoginDto user) {
-        UserDetails principal = userService.loadUserByUsername(user.getUsername());
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(principal, user.getPassword(), principal.getAuthorities());
-        Authentication authentication = daoAuthenticationProvider.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        Instant now = Instant.now();
-        long expiry = 36000L;
-        String scope = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(" "));
-
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiry))
-                .subject(authentication.getName())
-                .claim("scope", scope)
-                .build();
-
-        return new ResponseEntity<>(this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue(),
-                HttpStatus.OK);
+        return new ResponseEntity<>(authService.authenticate(user), HttpStatus.OK);
     }
 }
